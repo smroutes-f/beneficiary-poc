2a64a6ad721acd976e258033f4ebce5c9f21609e
smroutes <npmstartdev@gmail.com>
2024-01-24 18:08:37 +0530

    feat: fix the back button issue

--- src/app/components/beneficiary/beneficiary.component.ts
+++ src/app/components/beneficiary/beneficiary.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CapitalizePipe } from '@app/utils/capitalize.pipe';
 import { BeneficiariesComponents, BeneficiaryTypes } from '@app/utils/constants';
@@ -16,7 +16,7 @@ import { BeneficiariesComponents, BeneficiaryTypes } from '@app/utils/constants'
   templateUrl: './beneficiary.component.html',
   styleUrl: './beneficiary.component.scss'
 })
-export class BeneficiaryComponent {
+export class BeneficiaryComponent implements AfterViewInit {
   @Input() fieldIndex: number = 0;
   @Input() items: FormGroup = new FormGroup({});
   @ViewChild('otherFormFields', { read: ViewContainerRef }) myContainerRef: ViewContainerRef | undefined;
@@ -28,6 +28,11 @@ export class BeneficiaryComponent {
     this.allBeneficiaryTypes = Array.from(Object.values(BeneficiaryTypes));
   }
 
+  ngAfterViewInit() {
+    if(!this.items.get('type')?.value) return;
+    this.loadDynamicComponent(this.items.get('type')?.value, this.fieldIndex);
+  }
+
   onSelectChange(event: any) {
     const selectedValue = event.target.value;
     if(selectedValue === 'null') {
@@ -35,7 +40,7 @@ export class BeneficiaryComponent {
       return;
     }
 
-    this.loadDynamicComponent(selectedValue, this.fieldIndex)
+    this.loadDynamicComponent(selectedValue, this.fieldIndex);
   }
 
   removeComponent() {
@@ -47,6 +52,7 @@ export class BeneficiaryComponent {
 
   loadDynamicComponent(type: string, arrayIndex: number) {
 
+    console.log("called");
     this.removeComponent();
 
     this.otherFields = this.myContainerRef?.createComponent(
